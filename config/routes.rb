8b06e4701f1f3dd3d85371319bb8d02d035fb367
Boehler Frédéric <fred@all-pixels.com>
2011-07-05 20:07:04 +0200

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -51,6 +51,7 @@ SeasonsProject::Application.routes.draw do
   # just remember to delete public/index.html.
   root :to => 'templates#index'
 
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
