5f594b7c0f6fceed9293d46ea8c9aa77738a591a
Yann Klis <yann.klis@novelys.com>
2011-07-09 16:13:13 +0200

    added Google Chrome Frame support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def is_older_than_ie8
+    @is_older_than_ie8 ||= request.env['HTTP_USER_AGENT'].downcase =~ /msie/i && !request.env['HTTP_USER_AGENT'].downcase.index('msie 9')
+  end
 end
